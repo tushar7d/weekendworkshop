612315ddf36e8503d51bf43c746ce5861a520c47
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-05-07 16:53:03 +0530

    added footers

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,11 @@
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/themeprovider";
-
+import { ModeToggle } from "@/components/modetoggle";
 import { cn } from "@/lib/utils";
-
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -11,9 +13,61 @@ const fontSans = FontSans({
 
 export const metadata = {
   title: "Weekend Workshops",
-  description: "Learn product designs the easy way",
+  description: "Learn something new every weekend",
 };
 
+function MountainIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  );
+}
+
+let Header = () => {
+  return (
+    <nav className="z-50 bg-background sticky top-0   ">
+      <div className="flex justify-between border-b p-4">
+        <div className="text-foreground text-2xl  font-medium">W</div>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+};
+let Footer = () => {
+  return (
+    <footer className=" bg-background z-50  mt-12 border-t w-full flex justify-between items-center">
+      <div className="p-12">
+        <div className="text-4xl mb-2 font-medium">
+          W
+        </div>
+      <div className="text-foreground/70 font-medium ">
+        Copyright 2024 · Weekend Workshop
+      </div>
+
+      </div>
+      
+      <div className="pr-12"> 
+        <div className="text-xl">Subscribe to our newsletter</div>
+        <div className="pt-4 text-foreground flex space-x-3 ">
+          <Input type="email" placeholder="Enter Email" />
+          <Button>Subscribe</Button>
+        </div>
+      </div>
+    </footer>
+  );
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -22,16 +76,17 @@ export default function RootLayout({ children }) {
           attribute="class"
           defaultTheme="dark"
           enableSystem
-      
           disableTransitionOnChange
         >
           <div
             className={cn(
-              "min-h-screen bg-background font-sans antialiased",
+              "bg-background font-sans antialiased",
               fontSans.variable
             )}
           >
+            <Header />
             {children}
+            <Footer />
           </div>
         </ThemeProvider>
       </body>
--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
-import { ModeToggle } from "@/components/modetoggle";
-
-
 export default function Home() {
+  let heading = "Weekends for the win";
+  let description = `Join our weekend workshops on product design, product management, and more`;
+
   return (
-    <main className=" flex space-x-3 align-middle  items-center justify-center w-screen h-screen">
-      <div className="text-4xl font-semibold">Coming soon</div>
-      <ModeToggle />
+    <main className=" flex space-x-3 align-middle  items-center justify-center">
+      <div className="z-10 flex h-full w-full flex-col items-center justify-center">
+        <h1 className="mx-6 mt-12 w-[300px] bg-gradient-to-b from-black/80 to-black bg-clip-text pb-4  text-center text-5xl font-extrabold leading-tight text-transparent dark:from-white dark:to-[#AAAAAA] md:!w-full lg:!mt-20 lg:text-6xl xl:leading-snug">
+          {heading}
+        </h1>
+        <p className="font-space-grotesk mx-6 max-h-[112px] w-[315px] text-center text-xl text-[#666666] dark:text-[#888888] md:max-h-[96px] md:w-[660px] md:text-2xl">
+          {description}
+        </p>
+      
+      </div>
     </main>
   );
 }
