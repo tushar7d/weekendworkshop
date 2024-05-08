7a33cfe5dc36afdd817c167df7d565cbab03b355
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-05-08 15:23:51 +0530

    changes

--- app/events/design-systems-go/page.js
+++ app/events/design-systems-go/page.js
@@ -2,39 +2,38 @@ export default function Event() {
   return (
     <div className=" min-h-[80vh] p-12">
       <div className="mb-3 font-mono">Saturday · 25th May 2024</div>
-      <h1 className="mb-4 text-5xl font-semibold text-foreground">
+      <h1 className="mb-6 text-4xl md:text-5xl font-semibold text-foreground">
         Design System Go!
       </h1>
-      <p className="mb-4">
+      <p className="mb-6 text-xl md:text-2xl">
         Unleash your creativity and level up your design game! Prepare for a
         detailed Design System Workshop that will take your design skills to new
         heights! Discover the secret sauce of successful design systems as we
         dive deep into:
       </p>
-      <ul className="grid grid-cols-3 gap-3 mb-4 ">
-        <li className="flex p-3 text-sm border rounded-xl dark:bg-zinc-900">
-          <div className="mr-4 rounded-full size-12 bg-zinc-900 dark:bg-white/20"></div>
-          <div className="w-[80%] ">
+      <ul className="grid grid-cols-1 md:grid-cols-3 gap-3 mb-4 ">
+        <li className=" p-4 text-sm border rounded-xl dark:bg-zinc-900">
+          <div className="mb-4 rounded-full size-12 bg-zinc-50 dark:bg-white/20"></div>
+          <div className=" text-lg">
             Mastering Tokens to create harmonious visual languages and brand
             identities
           </div>
         </li>
-        <li className="flex p-3 text-sm border rounded-xl dark:bg-zinc-900">
-          <div className="mr-4 rounded-full size-12 bg-zinc-900 dark:bg-white/20"></div>
-          <div  className="w-[80%] " >
+        <li className=" p-4 text-sm border rounded-xl dark:bg-zinc-900">
+          <div className="mb-4 rounded-full size-12 bg-zinc-50 dark:bg-white/20"></div>
+          <div  className=" text-lg " >
             Crafting Components with precision, flexibility, and reusability
           </div>
         </li>
-        <li className="flex p-3 text-sm border rounded-xl dark:bg-zinc-900">
-          <div className="mr-4 rounded-full size-12 bg-zinc-900 dark:bg-white/20"></div>
-          <div  className="w-[80%] ">
+        <li className=" p-4 text-sm border rounded-xl dark:bg-zinc-900">
+          <div className="mb-4 rounded-full size-12 bg-zinc-50 dark:bg-white/20"></div>
+          <div  className=" text-lg ">
             Documenting your design system like a pro for seamless collaboration
-            & communication 
           </div>
         </li>
       </ul>
-      <p>
-        {" "}
+      <p className="mt-6 text-xl md:text-2xl">
+        
         Gain exclusive access to advanced techniques and industry secrets.
         Register now and join us on this thrilling journey
       </p>
