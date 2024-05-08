3c7b8d7cf690ed1b8cb5bfc12bb5bab69f469edf
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-05-08 17:37:07 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="flex items-center justify-center space-x-3 align-middle ">
         <div className="z-10 flex flex-col items-center justify-center w-full h-full">
           <img className="w-[22rem]" src="/cube.png" />
-          <h1 className="mx-6 mt-6 w-[300px] bg-gradient-to-b from-black/80 to-black bg-clip-text pb-4  text-center text-5xl font-extrabold leading-tight text-transparent dark:from-white dark:to-[#AAAAAA] md:!w-full  lg:text-6xl xl:leading-snug">
+          <h1 className="mx-3 mt-6 w-[340px] bg-gradient-to-b from-black/80 to-black bg-clip-text pb-4  text-center text-5xl font-extrabold leading-tight text-transparent dark:from-white dark:to-[#AAAAAA] md:!w-full  lg:text-6xl xl:leading-snug">
             {heading}
           </h1>
           <p className="font-space-grotesk mx-6 max-h-[112px] w-[315px] text-center text-xl text-[#666666] dark:text-[#888888] md:max-h-[96px] md:w-[660px] md:text-2xl">
@@ -19,14 +19,14 @@ export default function Home() {
         </div>
       </section>
       <section className="mx-3 mt-12 text-foreground ">
-        <div className=" mx-auto  max-w-[60rem] flex items-end justify-between border dark:border-0  dark:bg-zinc-900 p-6 rounded-2xl">
+        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between border dark:border-0  dark:bg-zinc-900 p-6 rounded-2xl">
           <div>
             <Badge className={"mb-4 "} variant="outline">
               Upcoming event
             </Badge>
-            <div className="my-3 font-mono">
-        Virtual · 25th May 2024 · 2:00 PM IST
-      </div>
+            <div className="mt-3 mb-1 font-mono text-sm">
+              Virtual · 25th May · 2:00 PM IST
+            </div>
 
             <h2 className="mb-2 text-2xl">Design System Go!</h2>
             <p className="">
@@ -34,24 +34,26 @@ export default function Home() {
               and effective communication
             </p>
           </div>
-          <Link href="/events/design-systems-go">
+          <Link href="/events/design-systems-go" className="mt-6 md:mt-0">
             <Button className="">Learn More</Button>
           </Link>
         </div>
       </section>
       <section className="mx-3 mt-6 text-foreground ">
-        <div className=" mx-auto  max-w-[60rem] flex items-end justify-between border dark:border-0  dark:bg-zinc-900 p-6 rounded-2xl">
+        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between border dark:border-0  dark:bg-zinc-900 p-6 rounded-2xl">
           <div>
             <Badge className={"mb-4 "} variant="outline">
               Planned event
             </Badge>
-
+            <div className="mt-3 mb-1 font-mono text-sm">
+              Virtual · 1st June
+            </div>
             <h2 className="mb-2 text-2xl">Protoyping with variables</h2>
             <p className="">
               Learn how to effectively use variables for building prototypes
             </p>
           </div>
-          <Link href="/">
+          <Link href="/" className="mt-6 md:mt-0">
             <Button className="">Learn More</Button>
           </Link>
         </div>
