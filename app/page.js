86ea6dc4ccd517161380b956253c067147517705
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-05-19 11:14:01 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -25,10 +25,6 @@ export default function Home() {
           <p className=" mx-6  text-center text-xl text-[#666666] dark:text-[#888888]  md:text-2xl">
             {description}
           </p>
-          
-            
-           
-          
         </div>
       </section>
       <section className="mx-3 mt-12 ">
