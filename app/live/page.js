ae37b75705dd63f92eec8ead5695b7881f619fd6
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2024-06-16 19:36:01 +0530

    changes

--- app/live/page.js
+++ app/live/page.js
@@ -1,31 +1,14 @@
 import { Calendar } from "lucide-react";
 
 export default function Event() {
-    return (
-
-
-        <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
-
-            <div className="text-center ">
-                <Calendar className="mx-auto mb-6 size-20" />
-            <h1 className="mb-1 text-xl">Ask me anything</h1>
-            <h2 className="mb-3 text-3xl font-semibold ">Design systems Edition</h2>
-            </div>
-
-            <div>Event starts on <span className="text-green-500">Sunday 16 June, 8:00PM IST </span></div>
-
-        </div>
-        
-    )}
-
-
-
-
-/*<iframe
+  return (
+    <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
+      <iframe
         title="100ms-app"
         allow="camera *;microphone *;display-capture *"
-        src="https://weekendworkshop.app.100ms.live/meeting/blw-ajzs-eit"
+        src="https://weekendworkshop-confernece.app.100ms.live/meeting/gnt-dsvs-igf"
         className="w-full h-screen "
-    >
-    </iframe>)*/
-        
+      ></iframe>
+    </div>
+  );
+}
