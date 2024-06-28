1cf2ad3c1b463a64b08c5ee696b08a9110fd59af
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2024-06-29 01:31:22 +0530

    changes

--- app/live/page.js
+++ app/live/page.js
@@ -8,15 +8,15 @@ export default function Event() {
 
     <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
      
-    <div className="text-center ">
+    <div className="mt-6 text-center">
         <Calendar className="mx-auto mb-6 size-20" />
     
     <h2 className="mb-3 text-3xl font-semibold ">Portfolio Review</h2>
     </div>
 
-    <div>Event starts on <span className="text-green-500">Sunday 28 June, 8:00PM IST </span></div>
-    <img src="/pr.png" className="p-3 max-w-[512px] mt-6 border w-fit rounded-xl" />
-    <div className="p-6 mt-4 font-medium text-center border rounded-xl">
+    <div className="w-full p-3 mx-2 text-center">Event starts on <span className="text-green-500 ">Sunday 28 June, 8:00PM IST </span></div>
+    <img src="/pr.png" className="max-w-[500px] w-full mx-3 mt-4 border rounded-xl" />
+    <div className="p-6 mx-3 mt-4 font-medium text-center border rounded-xl w-fit ">
       <div className="text-xl ">As we can only review limited number of portfolios please book your slot</div> 
       <Link
             href="https://forms.gle/MoZ8VTimErmy2KbE7"
