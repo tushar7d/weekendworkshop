d5fe02374bdfc81f1009f283c95c06066a7823a5
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2024-06-29 00:37:20 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -4,12 +4,40 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Logo from "./cube.png";
 import Link from "next/link";
-export default function Home() {
+
+
+
+export default async function Home() {
   let heading = "Weekends for the win";
   let description = `Join our weekend workshops on product design, product management, and more`;
-
+  const data = await fetch(
+    "https://ap-south-1.cdn.hygraph.com/content/clxymi2ch01cc07w4j39fgpyb/master",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: `query Events {
+  events {
+    description
+    formLink
+    type
+    name
+    state
+    date
+  }
+}`,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  ).then((res) => res.json());
+  let events = data.data.events
+ 
+  console.log(events)
   return (
+
     <>
+   
       <section className="flex items-center justify-center space-x-3 align-middle ">
         <div className="flex flex-col items-center justify-center w-full h-full p-3 ">
           <Image
@@ -27,129 +55,59 @@ export default function Home() {
           </p>
         </div>
       </section>
-      <section className="mx-3 mt-6 ">
-        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between    border p-6 rounded-2xl">
-          <div>
-            <Badge className={"mb-4 "} variant="outline">
-              Planned
-            </Badge>
-            <div className="mt-3 mb-1 font-mono text-sm">
-              Virtual · 29th June
+      <div>
+      {events.map((e,key)=>{
+        return(
+
+          <section key={key} className="mx-3 mt-6 ">
+          <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between    border p-6 rounded-2xl">
+            <div>
+              <Badge className={"mb-4 "} variant="outline">
+               {e.state}
+              </Badge>
+              <div className="mt-3 mb-1 font-mono text-sm">
+              {e.type} · {e.date}
+              </div>
+              <h2 className="mb-2 text-2xl">{e.name}</h2>
+              <p className=" opacity-60">
+              {e.description}
+              </p>
+            </div> 
+            <div className="flex items-end space-x-3 md:space-y-3 md:flex-col">
+            <Link
+              href="/live"
+              className="mt-6 md:mt-0 w-[100px]"
+            >
+              <Button variant="outline" className="w-full ">
+                Join
+              </Button>
+            </Link>
+            {e.formLink?<Link
+              href="https://forms.gle/MoZ8VTimErmy2KbE7"
+              target="_blank"
+              className="mt-6 md:mt-0 w-[100px]"
+            >
+              <Button variant="outline" className="w-full">
+                Book Slot 
+              </Button>
+            </Link>:null}
+            
             </div>
-            <h2 className="mb-2 text-2xl">Portfolio Review</h2>
-            <p className=" opacity-60">
-              Showcase your portfolio and get expert inputs
-            </p>
-          </div> 
-          <div className="flex items-end space-x-3 md:space-y-3 md:flex-col">
-          <Link
-            href="/live"
-            className="mt-6 md:mt-0 w-[100px]"
-          >
-            <Button variant="outline" className="w-full ">
-              Join
-            </Button>
-          </Link>
-          <Link
-            href="https://forms.gle/MoZ8VTimErmy2KbE7"
-            target="_blank"
-            className="mt-6 md:mt-0 w-[100px]"
-          >
-            <Button variant="outline" className="w-full">
-              Book Slot 
-            </Button>
-          </Link>
+            
           </div>
-          
-        </div>
-      </section>
+        </section>
+        )
+      })}
+      </div>
+   
+     
       
     
-      <section className="mx-3 mt-6 ">
-        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between    border p-6 rounded-2xl">
-          <div>
-            <Badge className={"mb-4 "} variant="outline">
-              Planned
-            </Badge>
-            <div className="mt-3 mb-1 font-mono text-sm">
-              Virtual · 7th July
-            </div>
-            <h2 className="mb-2 text-2xl">Prototyping with variables</h2>
-            <p className=" opacity-60">
-              Learn how to effectively use variables for building prototypes
-            </p>
-          </div>
-          <Link
-            href="/events/prototyping-with-variables"
-            className="mt-6 md:mt-0"
-          >
-            <Button variant="outline" className="">
-              Learn More
-            </Button>
-          </Link>
-        </div>
-      </section>
-
-      <section className="mx-3 mt-6 ">
-        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between    border p-6 rounded-2xl">
-          <div>
-            <Badge className={"mb-4 "} variant="outline">
-              Planned
-            </Badge>
-            <div className="mt-3 mb-1 font-mono text-sm">
-              Virtual · 14th July
-            </div>
-            <h2 className="mb-2 text-2xl">Figma Plugin 101</h2>
-            <p className="opacity-60">
-              Learn how to build internal tools with open source Figma Plugins.
-              More details to come.
-            </p>
-          </div>
-        </div>
-      </section>
-      
-      <section className="mx-3 mt-12 ">
      
-        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between   border p-6 rounded-2xl">
-          <div>
-            <Badge className={"mb-4 "} variant="outline">
-              Past
-            </Badge>
-            <div className="mt-3 mb-1 font-mono text-sm">
-              Virtual · 25th May · 2:00 PM IST
-            </div>
 
-            <h2 className="mb-2 text-2xl">Design System Go!</h2>
-            <p className=" opacity-60">
-              Unlock secrets for consistent visual identity, reusable designs,
-              and effective communication
-            </p>
-          </div>
-          <Link href="/events/design-systems-go" className="mt-6 md:mt-0">
-            <Button variant="outline" className="">
-              Learn More
-            </Button>
-          </Link>
-        </div>
-      </section>
-
-      <section className="mx-3 mt-6 ">
-        <div className=" mx-auto  max-w-[60rem] flex flex-col md:flex-row items-start md:items-end justify-start md:justify-between    border p-6 rounded-2xl">
-          <div>
-            <Badge className={"mb-4 "} variant="outline">
-              Past
-            </Badge>
-            <div className="mt-3 mb-1 font-mono text-sm">
-              Virtual · 16th June
-            </div>
-            <h2 className="mb-2 text-2xl">Design Systems AMA</h2>
-            <p className=" opacity-60">
-             Ask me anything about design systems
-            </p>
-          </div>
-          
-        </div>
-      </section>
+      
+      
+    
     </>
   );
 }
