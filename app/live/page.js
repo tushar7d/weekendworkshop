03e569ca3969abfdbc15f223d9d2053ea688c879
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2024-06-28 10:30:09 +0530

    changes

--- app/live/page.js
+++ app/live/page.js
@@ -2,13 +2,31 @@ import { Calendar } from "lucide-react";
 
 export default function Event() {
   return (
+
+
     <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
+
+    <div className="text-center ">
+        <Calendar className="mx-auto mb-6 size-20" />
+    
+    <h2 className="mb-3 text-3xl font-semibold ">Portfolio Review</h2>
+    </div>
+
+    <div>Event starts on <span className="text-green-500">Sunday 28 June, 8:00PM IST </span></div>
+
+</div>
+   
+  );
+}
+
+
+/*
+ <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
       <iframe
         title="100ms-app"
         allow="camera *;microphone *;display-capture *"
         src="https://weekendworkshop-confernece.app.100ms.live/meeting/gnt-dsvs-igf"
         className="w-full h-screen "
       ></iframe>
-    </div>
-  );
-}
+    </div> */
+    
