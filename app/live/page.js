a0c8cee9c0a6e105581a9eaf9e52e7e369d48460
Tushar <41472705+tushar7d@users.noreply.github.com>
2024-10-26 12:35:14 +0530

    cahnegs

--- app/live/page.js
+++ app/live/page.js
@@ -13,21 +13,10 @@ export default function Event() {
     <h2 className="mb-3 text-3xl font-semibold ">Framer 101</h2>
     </div>
 
-    <div className="w-full p-3 mx-2 text-center">Event starts on <span className="text-green-500 ">Saturday 26th October, 7:00PM IST </span></div>
-    <img src="/framer.jpg" className="max-w-[500px] w-full mx-3 mt-4 border rounded-xl" />
-    <div className="p-6 mx-3 mt-4 font-medium text-center border rounded-xl w-fit ">
-      <div className="text-xl ">please book your slot</div> 
-      <Link
-            href="https://forms.gle/jkqufrhk62tUEA2Z7"
-            target="_blank"
-            className="mt-6 md:mt-0 w-[100px]"
-          >
-            <Button  className="w-full mt-6">
-              Book Slot 
-            </Button>
-          </Link>
-      </div>
+    <div className="w-full p-3 mx-2 text-2xl text-center">Event starts on <span className="text-green-500 ">Saturday 26th October, 7:00PM IST </span></div>
+    <div>You will be able to view the event here on this page 15 mins before the event starts</div>
     
+  
 
 
 </div>
@@ -41,7 +30,7 @@ export default function Event() {
 
 
 /*
-   <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
+     <div className="flex flex-col items-center justify-center w-screen  min-h-[400px]">
       <iframe
         title="100ms-app"
         allow="camera *;microphone *;display-capture *"
@@ -49,6 +38,7 @@ export default function Event() {
         className="w-full h-screen "
       ></iframe>
     </div>
+    
 
   */
     
